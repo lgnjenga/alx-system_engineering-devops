e8b2c811348ed64c53845499f0851e1e716b1919
lgnjenga <lgnjenga@gmail.com>
2024-06-21 05:58:38 +0300

    Task 0 Correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,6 +16,6 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
-        return subscribers
+        return (subscribers)
     else:
-        return 0
+        return (0)
