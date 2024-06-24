3c0025327e267afe333e25837324a5910c2d270e
lgnjenga <lgnjenga@gmail.com>
2024-06-24 05:03:28 +0300

    Strace tmux debugging apache server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,6 @@
-# 0-strace_is_your_friend.pp
+# Fixes Apache 500 error
 
-# Ensure the directory for the file exists
-file { '/var/www/html':
-  ensure => 'directory',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Ensure the PHP file exists with correct permissions
-file { '/var/www/html/index.php':
-  ensure  => 'file',
-  content => '<?php phpinfo(); ?>',
-  mode    => '0644',
-  owner   => 'www-data',
-  group   => 'www-data',
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  require => File['/var/www/html/index.php'],
-}
-
