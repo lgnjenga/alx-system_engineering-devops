245a8cb3552678a195ab4515d9136973b4fa37be
lgnjenga <lgnjenga@gmail.com>
2024-06-28 00:12:35 +0300

    Web Stack Debugging 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# handle multiple concurrent requests in Nginx
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
